8834ca7ea710c22e8f564a505b3b7363dbc81f6e
Mohit <mohit1k234@gmail.com>
2025-09-19 09:41:33 +0530

    18/09/2025

--- 02_conditionals/07_sol.py
+++ 02_conditionals/07_sol.py
@@ -1,1 +1,10 @@
-print("hello")
+order_size = "medium"
+extra_shot = True
+
+if extra_shot:
+    coffee = order_size + " coffee with an extra_shot"
+
+else:
+    cofee = order_size + " coffee"
+
+print("order: ", coffee)
