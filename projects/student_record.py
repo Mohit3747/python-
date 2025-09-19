e551a2d9478f76544d11cf3373b94072c0a69d59
Mohit <mohit1k234@gmail.com>
2025-09-19 14:53:03 +0530

    19/09/2025

--- projects/student_record.py
+++ projects/student_record.py
@@ -1,25 +1,3 @@
-n=int(input("enter no of rows"))
-for i in range(n):
-    for j in range(n):
-        if j==0 or j==n-1:
-            print("*" ,end=" ")
-        else:
-            print(" ",end=" ")
-        print(
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 import os
 
 # File to store student data
